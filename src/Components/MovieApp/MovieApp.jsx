6968aa18fc2cc8348458d5e69124645ff9231bd7
Alisa L <lisaferistiii@gmail.com>
2024-08-14 20:21:45 +0300

    first commit

---
+++ src/Components/MovieApp/MovieApp.jsx
@@ -0,0 +1,10 @@
+import './MovieApp.css'
+import { Component } from 'react'
+
+class MovieApp extends Component {
+  render() {
+    return <div className="MovieApp" />
+  }
+}
+
+export default MovieApp
